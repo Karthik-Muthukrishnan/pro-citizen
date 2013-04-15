7803256324ad81a27e74f8ed07083ac920f5d294
Karthik Muthukrishnan <reachkarti@gmail.com>
2013-04-15 23:53:45 +0530

    static_pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	# returns full title on per-page basis
+	def full_title(page_title)
+		base_title = "Know your neighborhood"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end  
+	end
 end
