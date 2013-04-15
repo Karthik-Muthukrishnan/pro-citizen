255d1662d17b8e770b6640bbc55c1b53ccd56b7a
Karthik Muthukrishnan <reachkarti@gmail.com>
2013-04-15 21:45:24 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProCitizen::Application.config.secret_token = '9c6bc6767703058afe302e53c13c68c8f29f3f90d9cdd363b8d1a799ea055ad1290d824b61eeb370cb471978bd14f206e8677685ed9a25fc1e41088dc2b9885c'
