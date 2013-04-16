da462a29647b4b367dec582d2bd2d281bc020274
Karthik Muthukrishnan <reachkarti@gmail.com>
2013-04-16 23:27:34 +0530

    signin features

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def new
+  	@user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to Localite!"
+      # Handle a successful save.
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 ProCitizen::Application.routes.draw do
+ 
+  resources :users 
+  resources :sessions, only: [:new, :create, :destroy]
   
   root to: 'static_pages#home'
   
   match '/about', to: 'static_pages#about'
   match '/help', to: 'static_pages#help'
+  match '/signup',  to: 'Users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
